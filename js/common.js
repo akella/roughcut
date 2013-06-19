c65bb00e238098cc721ec4cc6da8aacb086c3e73
akella <akella.a@gmail.com>
2013-06-20 02:34:49 +0300

    fixes for 30 issues 2: Resurrection

--- js/common.js
+++ js/common.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$('.timeline__comment').click(function (e) {
 		$(this).children('.timeline__commenttext').toggleClass('is-visible');
 	});
+
+	$('.waveform__video').click(function (e) {
+		$('body').toggleClass('is-visiblevideo')
+	});
 });
