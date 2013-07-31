5737e5ac7f9af9f0f2ec28e243c4a0cd89763db0
akella <akella.a@gmail.com>
2013-07-31 16:55:43 +0300

    videocontrols toggle + videopreview

--- js/common.js
+++ js/common.js
@@ -5,13 +5,15 @@ $(document).ready(function() {
 	$('.timeline__comment').click(function (e) {
 		$(this).children('.timeline__commenttext').toggleClass('is-visible');
 	});
-
+	$('.rough__togglevideocontrols').click(function (e) {
+		$('.rough').toggleClass('is-toggledcontrols')
+	});
 	$('.waveform__video').click(function (e) {
 		$('body').toggleClass('is-visiblevideo')
 	});
 	$('.rough').click(function (e) {
 		//$('.rough').toggleClass('is-withleft is-withright')
-		$('.rough').toggleClass('is-fullscreencutlist');
+		$('.rough').addClass('is-fullscreencutlist');
 	});
 	waveformWrapper = document.querySelector(".waveform .waveform__wrap");
 	window.addEventListener("scroll", function() {
